c76cc538e62cdde0235468696571437354f959ba
Mahesh <maramai2@cisco.com>
2019-08-09 19:55:46 +0530

    port fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 let io = require('socket.io');
 const SocketManager = require('./server/model/SocketManager');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.Server(app);
 
@@ -17,5 +17,5 @@ io = io(server);
 
 server.listen(3000, () => {
   new SocketManager(io);
-  console.log(`Server listening on PORT ${PORT}`);
+  console.log(`Server listening on port ${port}`);
 });
